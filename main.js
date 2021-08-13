92208b55c0b5d4d6c1b8f5a996a8141e32b34da4
Justin Cockrell <justin.cockrell1@gmail.com>
2021-08-13 03:12:24 -0500

    Made it draw the chess board

--- main.js
+++ main.js
@@ -5,5 +5,14 @@ function init() {
     canvas =document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     ctx.fillRect(0,0,ctx.canvas.width,ctx.canvas.height);
-
+    var cellSize=ctx.canvas.width/8;
+    var count = 0;
+    for(var i = 0; i < 8; i++){
+        for(var j = 0; j < 8; j++){
+            ctx.fillStyle = count%2==0 ? "black" : "white"; 
+            ctx.fillRect(i*cellSize, j*cellSize, cellSize, cellSize);
+            count++;
+        }
+        count++;
+    }
 }
