003ff6dbc8a3efe0988b7e30f316cacb8aa05753
Justin Cockrell <justin.cockrell1@gmail.com>
2022-02-26 08:24:15 -0600

    add footer with copyright and change font

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ function handleScroll() {
     const header =document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY>0);
 }
+
+document.getElementById("date").textContent = new Date().getFullYear();
